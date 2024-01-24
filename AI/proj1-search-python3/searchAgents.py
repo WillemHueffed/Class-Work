4c625b1ed853367045dcf454ceba1f65a15ffa6c
WillemHueffed <willmhueffed@gmail.com>
2024-01-23 19:08:15 -0800

    p7 done

--- AI/proj1-search-python3/searchAgents.py
+++ AI/proj1-search-python3/searchAgents.py
@@ -422,36 +422,19 @@ class AStarFoodSearchAgent(SearchAgent):
         self.searchType = FoodSearchProblem
 
 def foodHeuristic(state, problem):
-    """
-    Your heuristic for the FoodSearchProblem goes here.
-
-    This heuristic must be consistent to ensure correctness.  First, try to come
-    up with an admissible heuristic; almost all admissible heuristics will be
-    consistent as well.
-
-    If using A* ever finds a solution that is worse uniform cost search finds,
-    your heuristic is *not* consistent, and probably not admissible!  On the
-    other hand, inadmissible or inconsistent heuristics may find optimal
-    solutions, so be careful.
-
-    The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a Grid
-    (see game.py) of either True or False. You can call foodGrid.asList() to get
-    a list of food coordinates instead.
-
-    If you want access to info like walls, capsules, etc., you can query the
-    problem.  For example, problem.walls gives you a Grid of where the walls
-    are.
-
-    If you want to *store* information to be reused in other calls to the
-    heuristic, there is a dictionary called problem.heuristicInfo that you can
-    use. For example, if you only want to count the walls once and store that
-    value, try: problem.heuristicInfo['wallCount'] = problem.walls.count()
-    Subsequent calls to this heuristic can access
-    problem.heuristicInfo['wallCount']
-    """
     position, foodGrid = state
-    "*** YOUR CODE HERE ***"
-    return 0
+    #Original code had caching of results in problem.heuristicInfo -> this lead to not enough pruning
+    #I think the issue is that the cost estimates weren't being updated for a given position after
+    #a food pellet had been eaten. I.e the heuristic for each coord was only calculated once when in
+    #reality it should have been updated whenever a pellet was eaten
+    distances = []
+    for pellet in foodGrid.asList():
+        distances.append(mazeDistance(position, pellet, problem.startingGameState))
+        #distances.append( ( abs(pellet[0] - position[0]) + abs(pellet[1] - position[1]) ) ) #this expands too many
+    if not len(distances): #same issue as before not sure why the heurstic gets called if all the pellets have been eaten
+        return 0
+    else:
+        return max(distances)
 
 class ClosestDotSearchAgent(SearchAgent):
     "Search for all food using a sequence of searches"
