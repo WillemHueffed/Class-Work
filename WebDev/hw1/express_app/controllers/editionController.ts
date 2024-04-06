e91f82b0b4a0be3fc1a19ad835715b82b4a67560
Willem Hueffed <willmhueffed@gmail.com>
2024-04-05 21:52:54 -0700

    almost done. 2 unit tests are stillf ucking up

--- WebDev/hw1/express_app/controllers/editionController.ts
+++ WebDev/hw1/express_app/controllers/editionController.ts
@@ -2,21 +2,25 @@ import { Request, Response } from "express";
 import { books, Edition } from "../data";
 
 export const add_edition = (req: Request, res: Response): void => {
-  const bookID = req.params.bookID as string;
+  const bookID = req.params.bookID;
   const book = books.find((book) => book.id === bookID);
   if (!book) {
     res.status(404).json({ error: "Book not found" });
     return;
   }
-  const { ed_number, pub_date } = req.body;
-  const edition = new Edition(ed_number, pub_date);
+  const { ed_num, pub_date } = req.body;
+  if (!ed_num || !pub_date) {
+    res.status(400).send({ error: "Please provide an ed_num and pub_date" });
+    return;
+  }
+  const edition = new Edition(ed_num, pub_date);
   book.editions.push(edition);
 
-  res.status(200).json({ book });
+  res.status(200).send(edition);
 };
 
 export const list_editions = (req: Request, res: Response): void => {
-  const bookID = req.params.id as string;
+  const bookID = req.params.bookID;
   const book = books.find((book) => book.id === bookID);
   if (!book) {
     res.status(404).json({ error: "Book not found" });
@@ -24,12 +28,20 @@ export const list_editions = (req: Request, res: Response): void => {
   }
   const editions = book.editions;
 
-  res.status(200).json({ editions });
+  res.status(200).send(editions);
 };
 
 export const delete_edition = (req: Request, res: Response): void => {
-  const bookID = req.params.id as string;
+  const bookID = req.params.bookID as string;
+  if (!bookID) {
+    res.status(400).send({ error: "bookID not present" });
+    return;
+  }
   const editionID = req.params.editionID as string;
+  if (!editionID) {
+    res.status(400).send({ error: "editionID not present" });
+    return;
+  }
   const book = books.find((book) => book.id === bookID);
   if (!book) {
     res.status(404).json({ error: "Book not found" });
@@ -44,5 +56,5 @@ export const delete_edition = (req: Request, res: Response): void => {
   }
   book.editions.splice(editionIndex, 1);
 
-  res.status(204);
+  res.status(204).end();
 };
