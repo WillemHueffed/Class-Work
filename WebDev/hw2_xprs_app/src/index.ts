57f35acf57d80f6781d526473f0122c7dcb701a5
Willem Hueffed <willmhueffed@gmail.com>
2024-04-11 13:11:19 -0700

    adds base for hw2

--- WebDev/hw2_xprs_app/src/index.ts
+++ WebDev/hw2_xprs_app/src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+// src/index.ts
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript with Express!");
 });
 
 app.listen(port, () => {
-  console.log();
+  console.log(`Server is running on http://localhost:${port}`);
 });
