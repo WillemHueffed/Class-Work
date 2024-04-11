e7932f11b5aeb0416866e921ef27d07d79a5a9ee
Willem Hueffed <willmhueffed@gmail.com>
2024-04-11 15:19:43 -0700

    Adds basic db connection in index.ts

--- WebDev/hw2_xprs_app/src/index.ts
+++ WebDev/hw2_xprs_app/src/index.ts
@@ -1,16 +1,46 @@
 // src/index.ts
+import { MongoClient } from "mongodb";
 import express from "express";
+import reviewRouter from "./routes/reviews";
 
 const app = express();
 const port = 3000;
 
-import reviewRouter from "./routes/reviews";
+const mongo_url = "mongodb://localhost:27017";
+const dbName = "my_db";
+
+async function connectToMongo() {
+  try {
+    await dbClient.connect();
+    console.log("Connected to MongoDB server");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+const dbClient = new MongoClient(mongo_url);
+connectToMongo();
+const database = dbClient.db(dbName);
 
 app.use("/reviews", reviewRouter);
 app.get("/", (req, res) => {
-  res.send("Hello, TypeScript with Express!");
+  res.send("Welcome to the reviews API with data persistance!");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// Close db connection on process termination
+process.on("SIGINT", async () => {
+  try {
+    await dbClient.close();
+    console.log("Disconnected from MongoDB");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error closing MongoDB connection:", error);
+    process.exit(1);
+  }
+});
+
+export { database };
