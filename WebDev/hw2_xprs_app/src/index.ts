f7da02a3ace5182d8b9fabce1517e1bced6223ae
Willem Hueffed <willmhueffed@gmail.com>
2024-04-20 19:44:37 -0700

    wa;lsdkjf;las

--- WebDev/hw2_xprs_app/src/index.ts
+++ WebDev/hw2_xprs_app/src/index.ts
@@ -23,11 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
+app.use("/reviews", reviewRouter);
 // error handler
 app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
@@ -50,6 +46,7 @@ const config = {
   clientID: process.env.AUTH0_CLIENTID,
   issuerBaseURL: process.env.AUTH0_ISSUERBASEURL,
 };
+
 app.use(auth(config));
 app.get("/", (req, res) => {
   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
@@ -84,10 +81,15 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.use("/reviews", reviewRouter);
 app.get("/", (req, res) => {
   res.send("Welcome to the reviews API with data persistance!");
 });
+
+// This is conflicting with the auth0 middleware...
+// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+// next(createError(404));
+//});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- WebDev/hw2_xprs_app/src/routes/reviews.ts
+++ WebDev/hw2_xprs_app/src/routes/reviews.ts
@@ -7,10 +7,10 @@ const router: Router = express.Router();
 
 router.use("/:reviewID/comments", commentRouter);
 
-router.post("/", requiresAuth, reviewController.create_review);
-router.post("/pub", reviewController.create_review);
-router.patch("/:reviewID", requiresAuth, reviewController.patch_review_by_id);
-router.patch("/:reviewIDpub", reviewController.patch_review_by_id);
+router.post(":bookID/", requiresAuth, reviewController.create_review);
+router.post(":bookID/pub", reviewController.create_review);
+router.patch(":reviewID", requiresAuth, reviewController.patch_review_by_id);
+router.patch("/:reviewID/pub", reviewController.patch_review_by_id);
 router.get("/byAuthor/:authorID", reviewController.get_reviews_by_authorID);
 router.get("/byBook/:bookID", reviewController.get_reviews_by_bookID);
 
