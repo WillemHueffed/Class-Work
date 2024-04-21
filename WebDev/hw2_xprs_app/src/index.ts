cb86887f5d5e899834a43666801d17a4edae6d9d
Willem Hueffed <willmhueffed@gmail.com>
2024-04-20 19:19:13 -0700

    adds stuff

--- WebDev/hw2_xprs_app/src/index.ts
+++ WebDev/hw2_xprs_app/src/index.ts
@@ -1,14 +1,44 @@
 // src/index.ts
 // https://manage.auth0.com/dashboard/us/dev-7qw8fvj8e40n61ha/
 import { UUID } from "bson";
+import logger from "morgan";
+import createError from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
 import { MongoClient, ServerApiVersion, Db } from "mongodb";
-import express from "express";
 import reviewRouter from "./routes/reviews";
 import { auth } from "express-openid-connect";
 import dotenv from "dotenv";
+import path from "path";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 const port = 3001;
 console.log(process.env.AUTH0_BASEURL);
 
