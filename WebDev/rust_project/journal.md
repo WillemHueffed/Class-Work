8d7ae8fac5ed8230e74912c3fa323e246f9b28f0
Willem Hueffed <willmhueffed@gmail.com>
2024-04-25 18:40:01 -0700

    initial commit for rust web dev project

---
+++ WebDev/rust_project/journal.md
@@ -0,0 +1,5 @@
+# 4/25
+- https://www.youtube.com/watch?v=L8tWKqSMKUI&list=PL2q9pua8FpiUwVll2Alk8W_d3eN8IxJjO&ab_channel=CodetotheMoon
+- https://actix.rs/docs/application
+Just doing initial project setup and playing around with the start documentation
+- example of mongodb with actix: https://github.com/actix/examples/tree/master/databases/mongodb
---
+++ WebDev/rust_project/src/main.rs
@@ -0,0 +1,62 @@
+mod model;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+//use mongodb::{
+//   bson::doc, options::ClientOptions, options::IndexOptions, Client, Collection, IndexModel,
+//};
+use mongodb::{options::ClientOptions, Client};
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Parse a connection string into an options struct.
+    let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+
+    // Manually set an option.
+    client_options.app_name = Some("My App".to_string());
+
+    // Get a handle to the deployment.
+    let client = Client::with_options(client_options)?;
+
+    // List the names of the databases in that deployment.
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }
+
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("localhost", 3002))?
+    .run()
+    .await
+}
