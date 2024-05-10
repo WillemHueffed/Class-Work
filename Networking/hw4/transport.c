f6a8b1ceef7a2cfd8c561a36423eac0dbbfb291f
Willem Hueffed <willmhueffed@gmail.com>
2024-05-10 13:41:20 -0700

    stuff kinda works now

--- Networking/hw4/transport.c
+++ Networking/hw4/transport.c
@@ -14,10 +14,14 @@
 #include "stcp_api.h"
 #include <arpa/inet.h>
 #include <assert.h>
+#include <inttypes.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#define WIN_SIZE 3072
 
 enum { CSTATE_ESTABLISHED }; /* you should have more states */
 
@@ -27,14 +31,24 @@ typedef struct {
 
   int connection_state; /* state of the connection (established, etc.) */
   tcp_seq initial_sequence_num;
-  tcp_seq send_seq_num;
-  tcp_seq recv_seq_num;
 
   /* any other connection-wide global variables go here */
+
+  char snd_wndw_buf[WIN_SIZE];
+  uint snd_wndw_f_ptr;
+  uint snd_wndw_b_ptr;
+  uint snd_seq_num;
+
+  char rcv_wndw_buf[WIN_SIZE];
+  uint rcv_wndw_f_ptr;
+  uint rcv_wndws_b_ptr;
+  uint rcv_seq_num;
+
 } context_t;
 
 static void generate_initial_seq_num(context_t *ctx);
 static void control_loop(mysocket_t sd, context_t *ctx);
+void our_dprintf(const char *format, ...);
 
 /* initialise the transport layer, and start the main loop, handling
  * any data from the peer or the application.  this function should not
@@ -48,73 +62,110 @@ void transport_init(mysocket_t sd, bool_t is_active) {
 
   generate_initial_seq_num(ctx);
 
-  if (is_active) {
-    STCPHeader SYNpacket;
-    SYNpacket.th_seq = ctx->initial_sequence_num;
-    ctx->send_seq_num = ctx->initial_sequence_num;
-    SYNpacket.th_off = 5;
-    SYNpacket.th_flags = TH_SYN;
-    SYNpacket.th_win = 3072;
-    printf("err1");
-    int status = stcp_network_send(sd, &SYNpacket, sizeof(STCPHeader));
-    if (status == -1) {
-      perror("send");
-    }
-    ctx->send_seq_num++;
+  if (is_active == TRUE) {
+    printf("in is active\n");
+    STCPHeader hdr;
+    memset(&hdr, 0, sizeof(STCPHeader));
 
-    STCPHeader activeRcvPacket;
-    printf("err2");
-    status = stcp_network_recv(sd, &activeRcvPacket, sizeof(STCPHeader));
-    if (status == -1) {
-      perror("recv");
-    }
-    // check for if SYN
-    ctx->recv_seq_num = activeRcvPacket.th_seq++;
+    hdr.th_off = htonl(5);
+    hdr.th_seq = htonl(ctx->initial_sequence_num);
+    printf("the initial sequence number is: %d\n", ctx->initial_sequence_num);
+    // hdr->th_ack = htonl(1);
+    hdr.th_flags |= htons(TH_SYN);
+    hdr.th_win = htonl(WIN_SIZE);
+    ctx->snd_seq_num = ctx->initial_sequence_num++;
+    our_dprintf("sender SYN: seq num %" PRIu32 "\n", ntohl(hdr.th_seq));
+    stcp_network_send(sd, &hdr, sizeof(STCPHeader), NULL);
 
-    STCPHeader ACKpacket;
-    ACKpacket.th_seq = ctx->send_seq_num;
-    ACKpacket.th_ack = ctx->recv_seq_num;
-    ctx->send_seq_num++;
-    ACKpacket.th_off = 5;
-    ACKpacket.th_flags = TH_ACK;
-    ACKpacket.th_win = 3072;
-    printf("err3");
-    status = stcp_network_send(sd, &ACKpacket, sizeof(STCPHeader));
-    if (status == -1) {
-      perror("send");
-    }
+    // Wait for SYN-ACK
+    memset(&hdr, 0, sizeof(STCPHeader));
+    stcp_network_recv(sd, &hdr, sizeof(STCPHeader));
+    hdr.th_off = ntohl(hdr.th_off);
+    hdr.th_seq = ntohl(hdr.th_seq);
+    hdr.th_ack = ntohl(hdr.th_ack);
+    hdr.th_flags |= ntohl(hdr.th_flags);
+    hdr.th_win = ntohl(hdr.th_win);
+    our_dprintf("sender received SYN-ACK: seq num: %d, ack num: %d\n",
+                hdr.th_seq, hdr.th_ack);
+
+    assert(ntohs(hdr.th_flags) & (TH_SYN | TH_ACK));
+    assert(ctx->snd_seq_num == hdr.th_ack - 1);
+    ctx->rcv_seq_num = ntohl(hdr.th_seq);
+
+    // Send the ACK
+    memset(&hdr, 0, sizeof(STCPHeader));
+    hdr.th_off = htonl(5);
+    hdr.th_seq = htonl(ctx->snd_seq_num++);
+    hdr.th_flags |= htons(TH_ACK);
+    hdr.th_win = htonl(WIN_SIZE);
+    hdr.th_ack = htonl(ctx->rcv_seq_num + 1);
+    stcp_network_send(sd, &hdr, sizeof(STCPHeader), NULL);
+    our_dprintf("sender sending ACK: seq num: %d, ack num: %d\n", hdr.th_seq,
+                hdr.th_ack);
+    // Should be safe to move state to ESTABLISHED
   } else {
-    STCPHeader passiveRcvPacket;
-    printf("err4");
-    int status = stcp_network_recv(sd, &passiveRcvPacket, sizeof(STCPHeader));
-    if (status == -1) {
-      perror("recv");
-    }
-    ctx->recv_seq_num = passiveRcvPacket.th_seq++;
-    // check if syn maybe
+    // How do I get evidence that this else statement runs???
+    char *ptr = NULL;
+    free(ptr);
+    free(ptr);
+    assert(0);
+    FILE *log_file;
+    char *log_filename = "logfile.txt";
 
-    STCPHeader SYN_ACKpacket;
-    SYN_ACKpacket.th_seq = ctx->initial_sequence_num;
-    ctx->send_seq_num = ctx->initial_sequence_num;
-    SYN_ACKpacket.th_off = 5;
-    SYN_ACKpacket.th_flags = TH_SYN | TH_ACK;
-    SYN_ACKpacket.th_win = 3072;
-    printf("err5");
-    status = stcp_network_send(sd, &SYN_ACKpacket, sizeof(STCPHeader));
-    if (status == -1) {
-      perror("send");
-    }
-    ctx->send_seq_num++;
+    // Open the log file in write mode ("w")
+    log_file = fopen(log_filename, "w");
 
-    STCPHeader finalACKpacket;
-    printf("err6");
-    status = stcp_network_recv(sd, &finalACKpacket, sizeof(STCPHeader));
-    if (status == -1) {
-      perror("recv");
-    }
-    ctx->recv_seq_num = finalACKpacket.th_seq++;
-    printf("end of ack");
+    // Write to the log file using fprintf()
+    fprintf(log_file, "This is a log message.\n");
+    fprintf(log_file, "Another log message.\n");
+
+    // Close the log file
+    fclose(log_file);
+    // get the SYN
+    // STCPHeader *hdr = (STCPHeader *)calloc(1, sizeof(STCPHeader));
+    our_dprintf("in receiver side\n");
+    STCPHeader hdr;
+    memset(&hdr, 0, sizeof(STCPHeader));
+    stcp_network_recv(sd, &hdr, sizeof(STCPHeader));
+    hdr.th_off = ntohl(hdr.th_off);
+    hdr.th_seq = ntohl(hdr.th_seq);
+    hdr.th_ack = ntohl(hdr.th_ack);
+    hdr.th_flags |= ntohl(hdr.th_flags);
+    hdr.th_win = ntohl(hdr.th_win);
+    assert(hdr.th_flags & TH_SYN);
+    our_dprintf("recvr received SYN: seq num: %d, ack num: %d\n", hdr.th_seq,
+                hdr.th_ack);
+
+    // Send the SYN-ACK
+    ctx->rcv_seq_num = hdr.th_seq + 1;
+    memset(&hdr, 0, sizeof(STCPHeader));
+    hdr.th_off = htonl(5);
+    hdr.th_seq = htonl(ctx->initial_sequence_num);
+    hdr.th_flags |= htons(TH_SYN | TH_ACK);
+    hdr.th_win = htonl(WIN_SIZE);
+    hdr.th_ack = htonl(ctx->rcv_seq_num);
+    stcp_network_send(sd, &hdr, sizeof(STCPHeader), NULL);
+    ctx->snd_seq_num = ctx->initial_sequence_num + 1;
+    our_dprintf("recvr sending SYN-ACK: seq num: %d, ack num: %d\n", hdr.th_seq,
+                hdr.th_ack);
+
+    // Receive the ACK
+    memset(&hdr, 0, sizeof(STCPHeader));
+    stcp_network_recv(sd, &hdr, sizeof(STCPHeader));
+    hdr.th_off = ntohl(hdr.th_off);
+    hdr.th_seq = ntohl(hdr.th_seq);
+    hdr.th_ack = ntohl(hdr.th_ack);
+    hdr.th_flags |= ntohl(hdr.th_flags);
+    hdr.th_win = ntohl(hdr.th_win);
+    our_dprintf("recvr received ACK: seq num: %d, ack num: %d\n", hdr.th_seq,
+                hdr.th_ack);
+
+    assert(hdr.th_ack == ctx->snd_seq_num + 1);
+    assert(hdr.th_flags == TH_ACK);
+
+    our_dprintf("finished passive side\n");
   }
+
   /* XXX: you should send a SYN packet here if is_active, or wait for one
    * to arrive if !is_active.  after the handshake completes, unblock the
    * application with stcp_unblock_application(sd).  you may also use
@@ -139,8 +190,10 @@ static void generate_initial_seq_num(context_t *ctx) {
   /* please don't change this! */
   ctx->initial_sequence_num = 1;
 #else
-  int seq_num = rand() % 255;
-  ctx->initial_sequence_num = seq_num;
+  /* you have to fill this up */
+  /*ctx->initial_sequence_num =;*/
+  srand(time(NULL));
+  ctx->initial_sequence_num = rand() % 256;
 #endif
 }
 
@@ -185,6 +238,8 @@ static void control_loop(mysocket_t sd, context_t *ctx) {
  * Calls to this function are generated by the dprintf amd
  * dperror macros in transport.h
  */
+
+/*
 void our_dprintf(const char *format, ...) {
   va_list argptr;
   char buffer[1024];
@@ -195,4 +250,25 @@ void our_dprintf(const char *format, ...) {
   va_end(argptr);
   fputs(buffer, stdout);
   fflush(stdout);
+}
+*/
+
+void our_dprintf(const char *format, ...) {
+  va_list argptr;
+  FILE *fp;
+
+  assert(format);
+
+  // Open the log file in append mode
+  fp = fopen("logfile", "a");
+  if (fp == NULL) {
+    fprintf(stderr, "Error opening log file.\n");
+    return;
+  }
+
+  va_start(argptr, format);
+  vfprintf(fp, format, argptr); // Write to the log file
+  va_end(argptr);
+
+  fclose(fp); // Close the log file
 }
