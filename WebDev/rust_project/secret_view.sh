bb530a00af1d977c5fdd4f50b2c26a7b3e3e6f3e
Willem Hueffed <willmhueffed@gmail.com>
2024-05-22 17:45:07 -0700

    updates shell script to work right with tokens

--- WebDev/rust_project/secret_view.sh
+++ WebDev/rust_project/secret_view.sh
@@ -1,7 +1,6 @@
-curl -X POST \
+curl -X GET \
   -v \
   -H "Content-Type: application/json" \
-  -d '{"email": "wheuffed@seattleu.edu", 
-      "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IndoZXVmZmVkQHNlYXR0bGV1LmVkdSIsImV4cCI6MTcxNjQ4MDY3Nn0.AWNUlroZSafeaItCdJlMqmXM90nYDCnQCTXLjWVYJ_E"}' \
-  localhost:3002/get-token
+  -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IndoZXVmZmVkQHNlYXR0bGV1LmVkdSIsImV4cCI6MTcxNjQ4MDY3Nn0.AWNUlroZSafeaItCdJlMqmXM90nYDCnQCTXLjWVYJ_E" \
+  localhost:3002/secret-view
 
